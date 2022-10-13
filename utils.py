11243051947b5db0aceb7e72b46a9dd13fbcfae0
NikiDiCostanzo <niki.dicostanzo@stud.unifi.it>
2022-10-13 16:38:14 +0200

    Create file.txt

--- utils.py
+++ utils.py
@@ -5,21 +5,30 @@ import csv
 import numpy as np
 #estrarre bb e calcolare centro e poi scalare tra [0, 1], path ?
 from matplotlib import pyplot as plt
-# AQnnotations file : label_idx x_center y_center width height
+
 from ast import literal_eval
-# dimension image: 1280 x 720
+# dimension image: 1280 x 720 -> scalare tra [0,1]
 def create_ann():
-    with open('video02.csv', 'r') as csv_file:
+    with open('video01.csv', 'r') as csv_file:
         data = csv.reader(csv_file)
         for row in data:
             coord = literal_eval(row[1].replace('.', '').replace(' ', ','))
            # print(coord, '\n')
-            #print(coord)
-            #fig = plt.figure()
+
+            # Aonnotations file : label_idx x_center y_center width height
             center = [((coord[0][0] + coord[1][0]) /2) /1280, ((coord[0][1] + coord[2][1]) /2) /720]
             w = (coord[0][0] - coord[1][0]) /1280  # (xmax - xmin)
             h = (coord[0][1] - coord[2][1]) /720  # (ymax - ymin)
-            print(center, 'w: ', w, 'h: ', h, '\n')
+            #print(center, 'w: ', w, 'h: ', h, '\n')
+
+            # path frame
+            name_frame = 'labels/' + row[0].split('/')[2].split('.')[0] + '.txt'
+            #print(name_frame)
+            # label_idx x_center y_center width height
+            write = '0' + ' ' + str(center[0]) + ' ' + str(center[1]) + ' ' + str(w) + ' ' + str(h)
+            with open(name_frame, 'w') as f:
+                 f.write(write)
+
         # for i in range(len(coord)):
         #     plt.scatter(coord[i][0], coord[i][1])
         #     print(coord[i][0],coord[i][1], '\n')
