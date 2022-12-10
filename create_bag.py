a4b4be53b5ef862d5a92b45d8d29b44653c966d4
NikiDiCostanzo <niki.dicostanzo@stud.unifi.it>
2022-12-10 14:38:10 +0100

    Fix.

--- create_bag.py
+++ create_bag.py
@@ -1,30 +1,29 @@
 import argparse
 import os
-import glob
-#simulatore esim
-path_ros = '/home/ninad/sim_ws/src/rpg_esim/event_camera_simulator/esim_ros'
-#home\ninad\sim_ws\src\rpg_esim\event_camera_simulator\esim_ros\scripts
-#folder = '/tmp/cheetah_example/esmi_frame!=/'
-#folder = '/mnt/e/frame'
 
-def create_csv(folder):
+#simulatore esim
+#path_ros = '/home/ninad/sim_ws/src/rpg_esim/event_camera_simulator/esim_ros'
+
+
+def create_csv(folder, path_ros):
     dirs = os.listdir(folder)
     for d in sorted(dirs): #cartelle dei video
         path = folder + d + '/'
         os.system("python {0}/scripts/generate_stamps_file.py -i {1} -r 20.0".format(path_ros, path))
+
 
 def create_bag(folder):
     dirs = os.listdir(folder)
     for d in sorted(dirs): #cartelle dei video
         path = folder + d #cartella ai frame
         print(path)
-        #os.system("rostopic list")
         os.system("rosrun esim_ros esim_node --data_source=2 "
                   "--path_to_output_bag={0}/video.bag "
                   "--path_to_data_folder={0} "
                   "--ros_publisher_frame_rate=60 --exposure_time_ms=10.0 "
                   "--use_log_image=1 --log_eps=0.1 --contrast_threshold_pos=0.15 "
                   "--contrast_threshold_neg=0.15".format(path))
+
 
 def create_lunch(folder):
     dirs = os.listdir(folder)
@@ -41,6 +40,7 @@ def create_lunch(folder):
                 "</launch> ".format(path))    #0.02 per estrarre 20 fps
         f.close()
 
+
 def extract_event(folder):
     dirs = os.listdir(folder)
     for d in sorted(dirs):  # cartelle dei video
@@ -52,18 +52,20 @@ def extract_event(folder):
             os.makedirs(new_path)
         os.system('mv ~/.ros/frame*.jpg {0}'.format(new_path))
 
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Create event frame")
     parser.add_argument("--video", dest="video", default=None, help="Path of the video")
+    parser.add_argument("--ros", dest="path_ros", default=None, help="Path of esim_ros")
     args = parser.parse_args()
     print('Video: ', args.video)
-    # print('Create csv')
-    # create_csv(args.video)
-    # print('Create bag')
-    # create_bag(args.video)
-    # print('Create file lunch')
-    # create_lunch(args.video)
+    print('Create csv')
+    create_csv(args.video, args.path_ros)
+    print('Create bag')
+    create_bag(args.video)
+    print('Create file lunch')
+    create_lunch(args.video)
     print('Extract event')
     extract_event(args.video)
-   # main()
+
 
