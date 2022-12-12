24cf68a77729308413ed6e498674a32fcf0eb1be
NikiDiCostanzo <niki.dicostanzo@stud.unifi.it>
2022-12-12 17:36:32 +0100

    Fix.

--- split_dataset.py
+++ split_dataset.py
@@ -9,6 +9,7 @@ import os
 # ...
 # /video70_0600.png
 
+
 def create_txt(fol_event, fol_yolo, split_val, split_train):
     train, test, val = split(fol_event, split_val, split_train)
 
@@ -16,7 +17,7 @@ def create_txt(fol_event, fol_yolo, split_val, split_train):
     dirs = os.listdir(im)  #/YOLO../.. /images/..
     name1 = fol_yolo + 'train.txt'
     name2 = fol_yolo + 'valid.txt'
-    name3 = fol_yolo + 'tesst.txt'
+    name3 = fol_yolo + 'test.txt'
 
     write_file(name1, dirs, train)
     write_file(name2, dirs, val)
@@ -28,7 +29,7 @@ def write_file(name, dirs, data):
         for d in sorted(dirs):  # cartelle dei video
             path = 'data/custom/images/' + d  # path_image
             if d.split('_')[0] in data:  # h
-                print(name, d)
+                #print(name, d)
                 f.writelines(path)
                 f.write('\n')
 
@@ -38,6 +39,7 @@ def split(folder, split_val, split_train):
     path_images = folder + 'images/'
     dirs = os.listdir(path_images)
     num = len(dirs)
+    print(split_train, split_val)
     len_train = round((split_train * num) / 100)
     len_val = round((split_val * num) / 100)
     len_test = num - (len_val + len_train)
@@ -47,28 +49,36 @@ def split(folder, split_val, split_train):
     count_a = []
     count = 0
     for d in sorted(dirs):  # ciclo su frame -> YOLO... /images
+       # print(d)
         if d.split('_')[0] not in fold_video:  # salvo il nome dei video che ho
-            if count > 0:
+            if count > 0: #metto il count precedente
                 count_a.append(count)
             fold_video.append(d.split('_')[0])
             count = 0
         else:
             count += 1
     count_a.append(count)  # ultimo altrimenti non si salva
-
+    print(count_a)
     index_train = 0
     index_val = 0
-    for d in count_a:
-        if index_train + d <= len_train:
+    tol = num / 100
+    for d in count_a:  # conta da 0_ vorrei che immaggini dello stesso video stanno insieme
+        if index_train + d <= len_train + tol:
             index_train = index_train + d
             index_val = index_train
-        elif len_train < index_val + d <= len_val + len_train:
+        elif len_train < index_val + d <= len_val + len_train + tol:
             index_val = index_val + d
+    print('Tol: ',tol)
 
+    if index_train == 0 or index_val == index_train:
+        # 60:100 = x : len(dirs)
+        index_train = round((len(dirs) * split_train)/100)
+        index_val = round((len(dirs) * split_val) / 100) + index_train  # prima ci sono quelli del train, va in ordine
     dirs = sorted(dirs)
+
     train = dirs[:index_train]
-    val = dirs[index_train + 1: index_val]
-    test = dirs[index_val + 1:]
+    val = dirs[index_train: index_val]
+    test = dirs[index_val:]
 
     print('test: ', len(test))
     print('train: ', len(train))
