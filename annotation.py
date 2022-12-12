8154aba1058809a7061f87437e3f48e73bd22b3d
NikiDiCostanzo <niki.dicostanzo@stud.unifi.it>
2022-12-12 17:54:33 +0100

    Fix.

--- annotation.py
+++ annotation.py
@@ -1,7 +1,6 @@
 import glob
 import csv
 import numpy as np
-# estrarre bb e calcolare centro e poi scalare tra [0, 1], path ?
 from matplotlib import pyplot as plt
 import argparse
 import os
@@ -14,43 +13,39 @@ from ast import literal_eval
 
 
 # cambio -> creo frame(togliendo il primo), ridimensiono, calcolo bb e salvo direttamente bb
-def data_yolo(folder, dest, size):
+def data_yolo(folder, dest, size_im):
     path_video = folder + 'video_cut/'
     dirs = os.listdir(path_video)
     no_bb = []
     for d in sorted(dirs):  # ciclo su video
-        new_path = folder + d.split('_')[0] + '/'  # dove salvo frame
+        new_path = folder + d.split('.')[0] + '/'  # dove salvo frame
         path = path_video + d
         if not os.path.exists(new_path):  # crea le cartelle dei frame
             os.makedirs(new_path)
 
         print('Extract frame RGB: ', d)
-        os.system("ffmpeg -i {0} -filter:v fps=fps=20 {1}/{2}_%14d.png".format(path, new_path, d.split('_')[0]))
+        os.system("ffmpeg -i {0} -filter:v fps=fps=20 {1}/{2}_%04d.png".format(path, new_path, d.split('_')[0]))
         dirs_frame = os.listdir(new_path)
         # resize_path = new_path + 'resize/'
         # if not os.path.exists(resize_path):  # crea le cartelle dei frame
         #     os.makedirs(resize_path)
         for f in sorted(dirs_frame):
             path_image = new_path + f
-            if 'png' == f.split('.')[1] and f.split('.')[
-                0] != 'video0001':  # non voglio il 1° frame perche non ho l'event
-                print('Scale_image: ', path_image)
-                #image_resize = resize_path + f + '.png'
-                #scale_image(path_image, image_resize, size)
+            if f.split('.')[0] != 'video0001':  # non voglio il 1° frame perche non ho l'event
                 print('Calculate bb')
                 bb = bounding_box(path_image)
                 print('Finish bb: ', bb)
-                if bb.size != 0:  # non salvo se non c'è bb
-                    name_file = d + f.split('.')[0]  # chiamo i file video#.txt
-                    create_ann(bb, name_file, dest, size)
+                if len(bb) != 0:  # non salvo se non c'è bb
+                    name_file = f.split('.')[0]  # chiamo i file video#.txt
+                    create_ann(bb, name_file, dest, size_im)
                 else:
                     no_bb.append(path_image)
                     # spostare imm. senza bb
                     print('Move images: ', f)
                     mv_image = dest + 'images/' + f
                     image_zero = dest + 'image_zero/'
-                    if not os.path.exists(  image_zero):  # crea le cartelle dei frame_zero
-                        os.makedirs(  image_zero)
+                    if not os.path.exists(image_zero):  # crea le cartelle dei frame_zero
+                        os.makedirs(image_zero)
                     os.system('mv {0} {1}'.format(mv_image, image_zero))
 
         print('Remove frames:', d)
@@ -61,18 +56,19 @@ def data_yolo(folder, dest, size):
         filewriter = csv.writer(csvfile)
         filewriter.writerow(no_bb)
 
-# uso video scalaati ! non serve
-def scale_image(frame, new_name, size):
+
+# uso video scalati ! non serve
+def scale_image(frame, new_name, size_im):
     img = cv2.imread(frame)
     print('Frame: ', frame)
-    img = cv2.resize(img, (size, size), interpolation=cv2.INTER_NEAREST)
+    img = cv2.resize(img, (size_im, size_im), interpolation=cv2.INTER_NEAREST)
     cv2.imwrite(new_name, img)
 
 # dimension image: 768 x 768 -> scalare tra [0,1]
-def create_ann(coord, row, dest, size):
-    center = [((coord[0][0] + coord[1][0]) / 2) / size, ((coord[0][1] + coord[2][1]) / 2) / size]
-    w = (coord[0][0] - coord[1][0]) / size  # (xmax - xmin)
-    h = (coord[0][1] - coord[2][1]) / size  # (ymax - ymin)
+def create_ann(coord, row, dest, size_im):
+    center = [((coord[0][0] + coord[1][0]) / 2) / size_im, ((coord[0][1] + coord[2][1]) / 2) / size_im]
+    w = (coord[0][0] - coord[1][0]) / size_im  # (xmax - xmin)
+    h = (coord[0][1] - coord[2][1]) / size_im  # (ymax - ymin)
     # print(center, 'w: ', w, 'h: ', h, '\n')
     print(coord)
     write = '0' + ' ' + str(center[0]) + ' ' + str(center[1]) + ' ' + str(w) + ' ' + str(h)
@@ -88,7 +84,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Create event frame")
     parser.add_argument("--video", dest="video", default=None, help="Path of the video")
     parser.add_argument("--dest", dest="dest", default=None, help="Path to save annotation")
-    parser.add_argument("--size", dest="size", default=None, help="Size of image")
+    parser.add_argument("--size_im", dest="size_im", default=768, help="Size of image")
     args = parser.parse_args()
     print('Video: ', args.video)
-    data_yolo(args.video, args.dest, args.size)
+    data_yolo(args.video, args.dest, args.size_im)
--- preprocessing.py
+++ preprocessing.py
@@ -8,11 +8,15 @@ def create_frame(folder, size):
     dirs = os.listdir(path_video)
     for d in sorted(dirs):  # ciclo su video
 
-        new_path = folder + 'video_cut/' + d.split('_')[0] + '/'
-        if not os.path.exists(new_path):  # crea le cartelle dei frame
-            os.makedirs(new_path)
+        new_path_video = folder + 'video_cut/'
+        if not os.path.exists(new_path_video):  # crea le cartelle dei video
+            os.makedirs(new_path_video)
 
-        path_cut = new_path + d.split('_')[0] + '.' + d.split('.')[1]
+        new_path_event = folder + 'event/' + d.split('.')[0] + '/'
+        if not os.path.exists(new_path_event):  # crea le cartelle dei frame
+            os.makedirs(new_path_event)
+
+        path_cut = new_path_video + d.split('.')[0] + '.' + d.split('.')[1]
         path = path_video + d
 
         print('Cut and scale video: ', d)
@@ -20,7 +24,7 @@ def create_frame(folder, size):
             "ffmpeg -i {0} -vf scale={2}:{2} -ss 00:00:01 -t 00:00:30 -async 1 -strict -2 {1}".format(path, path_cut,
                                                                                                       size))
         print('Extract frame: ', d)  # i frame li chiamo video#_##.png
-        os.system("ffmpeg -i {0} -filter:v fps=fps=20 {1}/{2}_%14d.png".format(path_cut, new_path, d.split('_')[0]))
+        os.system("ffmpeg -i {0} -filter:v fps=fps=20 {1}/{2}_%04d.png".format(path_cut, new_path_event, d.split('.')[0]))
 
 
 # rename video
@@ -42,7 +46,7 @@ def rename(folder):
 
 
 def rename_event(folder):
-    path = folder + 'event/'
+    path = folder + 'v/'
     dirs_event = os.listdir(path)
     print('folder', dirs_event)
     for d in sorted(dirs_event):  # cartelle video#
@@ -50,7 +54,7 @@ def rename_event(folder):
         print(path, 'd: ', d)
         path_frame = os.listdir(path_video)
         print('folder', path)
-        for f in reversed(path_frame):  # cartelle frame, ciclo dall'ultimo
+        for f in reversed(sorted(path_frame)):  # cartelle frame, ciclo dall'ultimo
             print('Frame: ', f)
             n = f.split('.')[0]
             num = str(int(n[len(n) - 4:len(n)]) + 2)
@@ -66,6 +70,7 @@ if __name__ == '__main__':
     parser.add_argument("--video", dest="video", default=None, help="Path of the video")
     parser.add_argument("--size", dest="size", default=768, help="Size of frames")
     args = parser.parse_args()
-    print('Rename video')
     rename(args.video)
     create_frame(args.video, args.size)
+
+    #rename_event(args.video)
